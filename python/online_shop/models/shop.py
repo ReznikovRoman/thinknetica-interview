506ec5fc47ff7e832826be13fd31d4ee9cd55a0a
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-02-20 11:16:42 +0300

    Add new method (add_product) to the Shop class

--- python/online_shop/models/shop.py
+++ python/online_shop/models/shop.py
@@ -1,21 +1,56 @@
+from .product import Product
+
 
 class Shop:
     """Shop where users can buy Products"""
     shops = []
 
     def __init__(self):
-        self._goods = []
+        self._products = []
         self._shop_id = len(Shop.shops) + 1
         Shop.shops.append(self)
 
-    def get_goods(self) -> list:
-        return self._goods
+    def get_products(self) -> list:
+        return self._products
 
-    def set_goods(self, new_goods: list):
-        if isinstance(new_goods, list):
-            self._goods = new_goods
+    def set_products(self, new_products: list):
+        if isinstance(new_products, list):
+            self._products = new_products
         else:
             print("Error! Goods must be list.")
+
+    def add_product(self, new_product: Product):
+        """
+        Adds a product to the Shop
+
+        Args:
+            new_product (Product): new Product that will be added
+
+        Returns:
+            None
+        """
+        if isinstance(new_product, Product):
+            self._products.append(new_product)
+        else:
+            print("Error! product must be Product.")
+
+    def remove_product(self, product: Product):
+        """
+        Removes a given product from the Shop
+
+        Args:
+            product (Product): product that will be removed
+
+        Returns:
+            None
+        """
+        if isinstance(product, Product):
+            if product in self._products:
+                self._products.remove(product)
+            else:
+                print(f"Error!Product {product.get_name()} is not in goods. ")
+        else:
+            print("Error! product must be Product.")
 
     def get_id(self) -> int:
         return self._shop_id
@@ -27,8 +62,8 @@ class Shop:
             print("Error! Shop ID must be int.")
 
     def print_products(self):
-        if len(self._goods) > 0:
-            for p in self._goods:
+        if len(self._products) > 0:
+            for p in self._products:
                 print(p)
         else:
             print("There are no products in the shop at the moment.")
@@ -38,14 +73,14 @@ class Shop:
         if _msg is not None:
             print(_msg)
 
-        for i, good in enumerate(self._goods, 1):
+        for i, good in enumerate(self._products, 1):
             print(f"{i} - {good.get_name()} ({good.get_price()})")
 
     def __str__(self):
-        return f"Goods: {self._goods}; \nShop ID: {self._shop_id}"
+        return f"Goods: {self._products}; \nShop ID: {self._shop_id}"
 
     def __repr__(self):
-        return f"(Goods: {self._goods}; Shop ID: {self._shop_id})"
+        return f"(Goods: {self._products}; Shop ID: {self._shop_id})"
 
 
 
