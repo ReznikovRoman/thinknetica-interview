66b4393039ab9b926d5a83ff742a70ff883d927b
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-02-21 11:21:15 +0300

    Add new fields to the CustomUser model admin

--- django/online_shop_web/accounts/admin.py
+++ django/online_shop_web/accounts/admin.py
@@ -2,14 +2,23 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from . import models
+from . import forms
 
 
 class CustomUserAdmin(BaseUserAdmin):
-    list_display = ('email', )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
     search_fields = ('email', )
     readonly_fields = ('id', 'date_joined', 'last_login')
     exclude = ('username', 'password', )
     ordering = ['email', ]
+
+    add_form = forms.CustomUserCreationForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
 
     filter_horizontal = ()
     list_filter = ()
