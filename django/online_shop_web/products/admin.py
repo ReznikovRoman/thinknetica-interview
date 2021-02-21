7372ea4c484a996e063e3ddb921679b141465df3
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-02-21 11:33:47 +0300

    Add custom model admin for the Product model

--- django/online_shop_web/products/admin.py
+++ django/online_shop_web/products/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    search_fields = ('name', )
+    readonly_fields = ('created_at', )
+
+
+admin.site.register(models.Product, ProductAdmin)
 
