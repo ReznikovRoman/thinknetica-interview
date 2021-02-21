62292388dcbcda22d621e041cc53aeb493017d05
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-02-21 11:36:43 +0300

    Add new fields (cart, created_at) to the Product model

--- django/online_shop_web/products/models.py
+++ django/online_shop_web/products/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from django.shortcuts import reverse
 
+from cart import models as cart_models
+
 
 class Product(models.Model):
+    cart = models.ManyToManyField(cart_models.Cart, related_name='products', blank=True)
     name = models.CharField(verbose_name="Product's name", max_length=255)
     price = models.IntegerField(
         verbose_name="Product's price",
@@ -11,6 +14,7 @@ class Product(models.Model):
             MinValueValidator(1),
         ]
     )
+    created_at = models.DateTimeField(verbose_name="date of product's creation", auto_now_add=True)
 
     def get_absolute_url(self):
         return reverse('products:product-detail', kwargs={'pk': self.pk})
